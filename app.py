ecdd0045a67e95c84a733a8dc1ae2cc83436e0d6
Ethan-Lem05 <88153797+Ethan-Lem05@users.noreply.github.com>
2024-01-27 21:41:28 -0500

    Update app.py
    - created models
    - started creating routes

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////test.db'
 db = SQLAlchemy(app)
 
+#################### MODELS ####################
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
@@ -12,10 +14,39 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+    
+class Conversation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    participants = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    messages = db.relationship('Message', backref='conversation', lazy=True)
+
+    def __repr__(self):
+        return '<Conversation %r>' % self.id
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sender = db.Column(db.String(80), nullable=False)
+    receiver = db.Column(db.String(80), nullable=False)
+    text = db.Column(db.String(80), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    conversation_id = db.Column(db.Integer, db.ForeignKey('conversation.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Message %r>' % self.text
+
+#################### ROUTES ####################
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    redirect('/login')
+
+@app.route('/login') 
+def login():
+    if()
+    return render_template('login.html')
+
+@app.route("/dashboard")
 
 if __name__ == '__main__':
     db.create_all()
